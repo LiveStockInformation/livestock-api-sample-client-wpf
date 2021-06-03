7d4bc3a76eb91de248365903e2f8eb9678f7de3a
apim <sarfraz.malik@gmail.com>
2021-06-03 16:49:40 +0100

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Windows;
+using Microsoft.Identity.Client;
+
+namespace livestock_api_samples
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// B2C tenant name
+        /// </summary>
+        private static readonly string TenantName = ConfigurationManager.AppSettings["TenantName"];
+        private static readonly string Tenant = $"{TenantName}.onmicrosoft.com";
+        private static readonly string AzureAdB2CHostname = $"{TenantName}.b2clogin.com";
+
+        /// <summary>
+        /// ClientId for the application which initiates the login functionality (this app)  
+        /// </summary>
+        private static readonly string ClientId = ConfigurationManager.AppSettings["ClientId"];
+
+
+        /// <summary>
+        /// Should be one of the choices on the Azure AD B2c / [This App] / Authentication blade
+        /// </summary>
+        private static readonly string RedirectUri = $"https://{TenantName}.b2clogin.com/oauth2/nativeclient";
+
+        /// <summary>
+        /// From Azure AD B2C / UserFlows blade
+        /// </summary>
+        public static string PolicySignUpSignIn = ConfigurationManager.AppSettings["PolicySignUpSignIn"];
+        public static string PolicyEditProfile = "b2c_1_edit_profile";
+        public static string PolicyResetPassword = ConfigurationManager.AppSettings["PolicyResetPassword"];
+
+        /// <summary>
+        /// Note: AcquireTokenInteractive will fail to get the AccessToken if "Admin Consent" has not been granted to this scope.  To achieve this:
+        /// 
+        /// 1st: Azure AD B2C / App registrations / [API App] / Expose an API / Add a scope
+        /// 2nd: Azure AD B2C / App registrations / [This App] / API Permissions / Add a permission / My APIs / [API App] / Select & Add Permissions
+        /// 3rd: Azure AD B2C / App registrations / [This App] / API Permissions / ... (next to add a permission) / Grant Admin Consent for [tenant]
+        /// </summary>
+        public static string[] ApiScopes = { ConfigurationManager.AppSettings["ApiScopes"] };
+
+        /// <summary>
+        /// URL for API which will receive the bearer token corresponding to this authentication
+        /// </summary>
+        public static string ApiEndpoint = ConfigurationManager.AppSettings["APiEndpoint"];
+
+        // Shouldn't need to change these:
+        private static string AuthorityBase = $"https://{AzureAdB2CHostname}/tfp/{Tenant}/";
+        public static string AuthoritySignUpSignIn = $"{AuthorityBase}{PolicySignUpSignIn}";
+        public static string AuthorityEditProfile = $"{AuthorityBase}{PolicyEditProfile}";
+        public static string AuthorityResetPassword = $"{AuthorityBase}{PolicyResetPassword}";
+
+        public static IPublicClientApplication PublicClientApp { get; private set; }
+
+        static App()
+        {
+            PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
+                .WithB2CAuthority(AuthoritySignUpSignIn)
+                .WithRedirectUri(RedirectUri)
+                .WithLogging(Log, LogLevel.Info, false) // don't log P(ersonally) I(dentifiable) I(nformation) details on a regular basis
+                .Build();
+
+            TokenCacheHelper.Bind(PublicClientApp.UserTokenCache);
+        }
+
+        private static void Log(LogLevel level, string message, bool containsPii)
+        {
+            string logs = $"{level} {message}{Environment.NewLine}";
+            File.AppendAllText(System.Reflection.Assembly.GetExecutingAssembly().Location + ".msalLogs.txt", logs);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,36 @@
+﻿
+
+# Livestock API Samples: WPF application signing in users with Azure Active Directory B2C and calling a livestock Information systems API
+
+> This sample is using MSAL.NET 4.x. 
+
+
+This simple sample demonstrates how to use the [Microsoft Authentication Library (MSAL) for .NET] to get an access token and call a Livestock Information Systems API secured by Azure AD B2C.
+
+
+### Run the project
+
+(1) Open the `livestock-api-sample-client-wpf` project. 
+(2) Set as startup Project 
+(3) Add folloiwng required values in App.cofig files:
+    (a) TenantName
+    (b) RedirectUrl
+    (c) CLientId
+    (d) PolicySignUpSignIn
+    (e) PolicyResetPassword
+    (f) ApiScopes
+    (g) APiEndpoint
+	(h) APISubscriptionKey 
+	
+and run the project. 
+
+
+The sample demonstrates the following functionality: 
+
+1. Click the sign-in button at the top of the application screen. The sample works exactly in the same way regardless of the account type you choose, apart from some visual differences in the authentication and consent experience. Upon successful sign in, the application screen will list some basic profile info for the authenticated user and show buttons that allow you to call an API and sign out.
+2. Close the application and reopen it. You will see that the app retains access to the API and retrieves the user info right away, without the need to sign in again.
+3. Sign out by clicking the Sign out button and confirm that you lose access to the API until the exit interactive sign in. 
+4. Call the api by clicking on the Call API buttom
+
+## More information
+For more information on Azure B2C, see [the Azure AD B2C documentation homepage](http://aka.ms/aadb2c). 
